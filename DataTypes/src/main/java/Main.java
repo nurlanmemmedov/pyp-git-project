05dc6e08f5af1744b42b813eb68a6ead6cfc01c6
[Asadullah Jamalov] <[asadullahnj@code.edu.az]>
2021-05-04 20:32:46 +0400

    asadullah added his code

--- DataTypes/src/main/java/Main.java
+++ DataTypes/src/main/java/Main.java
@@ -1,52 +1,98 @@
-import java.io.Console;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println(mergeOdds(1000));
+
+
+//        System.out.println("1-1000 arasi eded daxil edin");
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        if (a > 1 && a < 1000) {
+//            for (int i = 1; i <= a; i++) {
+//                if (i % 5 == 0 && sumNum(i) % 5 == 0) {
+//                    System.out.println(i);
+//                }
+//            }
+//        } else {
+//            System.out.println("Yanlish eded");
+//        }
+
+//        System.out.println(firstElem(52345));
+//        System.out.println(numOfElem(23456));
+//        System.out.println(powerOfElem(2, 3));
+//        System.out.println(firstToLast(7234345));
+
+        System.out.println(powerOfElem(2, 5));
+        System.out.println(findIndexElem(234, 3));
+        System.out.println(lastElem(234));
+        System.out.println(oddIndexNum(233784456));
+
+
     }
 
-/*    public static int findPower(int number, int power){
-        while(power > 0){
-            number = number * 10;
-            power = power -1;
-        }
-        return number;
-    }
+    /*  public static int sumNum(int num) {
+          int sum = 0;
+          while (num > 0) {
+              sum += (num % 10);
+              num = num / 10;
+          }
+          return sum;
+      }
+  */
 
-    public static int moveToEnd(int number){
-        var firstDigit = number;
-        var i = 0;
-        while (firstDigit > 10) {
-            i ++;
-            firstDigit = firstDigit / 10;
-        }
-        int newNumber = number - findPower(firstDigit, i);
-        var result = newNumber * 10 + firstDigit;
-        return result;
-    }*/
-
-    public static int findPower(int number, int power){
-        while(power > 0){
-            number = number * 10;
-            power = power -1;
-        }
-        return number;
-    }
-
-    public static int mergeOdds(int number){
-        var i = 0;
-        var k = 0;
+    public static int oddIndexNum(int num) {
         int result = 0;
-        while (number > 0) {
-            i ++;
-            if (i % 2 == 1){
-                result = result + findPower(number % 10, k );
-                k ++;
-            }
-            number = number / 10;
+        while (num > 10) {
+            result += lastElem(num);
+            result *= 10;
+            num /= 100;
         }
-        return  result;
+        return result/10;
     }
+
+
+    public static int firstToLast(int num) {
+        return (num - firstElem(num) * powerOfElem(numOfElem(num) - 1, 10)) * 10 + firstElem(num);
+    }
+
+
+    public static int firstElem(int num) {
+        while (num > 10) {
+            num /= 10;
+        }
+        return num;
+    }
+
+    public static int lastElem(int num) {
+        return num % 10;
+    }
+
+    public static int numOfElem(int num) {
+        int i = 0;
+        while (num > 0) {
+            num /= 10;
+            i++;
+        }
+        return i;
+    }
+
+    public static int findIndexElem(int num, int index) {
+        num %= powerOfElem(index, 10);
+
+        return num;
+
+    }
+
+
+    public static int powerOfElem(int power, int base) {
+        int result = 1;
+        for (int i = 0; i < power; i++) {
+            result *= base;
+        }
+        return result;
+    }
+
 
 }
+
